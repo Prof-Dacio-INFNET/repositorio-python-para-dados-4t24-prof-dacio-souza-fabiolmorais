cd4a11a5a3b1d31b4738e8876d25e2aabfce0eb2
Fabio Morais <fabio.lmorais99@gmail.com>
2024-11-01 18:00:25 -0300

    Pasta mais arquivo criados para fazer o TP1, Primeira questão resolvida

---
+++ fabio_morais_DR4_TP1/main.py
@@ -0,0 +1,10 @@
+# 1 - Crie uma lista chamada ‘usuarios’ que contenha ao menos 5 listas. Cada lista interna deve representar um usuário do INFwebNET com as seguintes informações: nome (string), idade (inteiro), cidade (string) e estado (string).
+
+usuarios = [
+  ["Fabio Morais", 25, "Caruaru", "Pernambuco"]
+  ["Cintia Silva", 25, "Caruaru", "Pernambuco"]
+  ["Maria do Socorro", 50, "Serra Talhada", "Pernambuco"]
+  ["Irineu Morais", 55, "Serra Talhada", "Pernambuco"]
+  ["Mayara Morais", 29, "Caruaru", "Pernambuco"]
+]
+
